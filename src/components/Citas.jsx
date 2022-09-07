2469a75f60844bb0f173f6a76e8c883d2e5392b3
carlosmsx <carlosmsx@gmail.com>
2022-09-06 23:48:01 -0300

    mejoras al maquetado, media queries, etc

--- src/components/Citas.jsx
+++ src/components/Citas.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import './citas.css'
 
 const Citas = () => {
     return (
-        <Card className="mt-4">
+        <Card className="mt-5">
             <Card.Header className="text-center"><h5>No hay citas</h5></Card.Header>
-            <Card.Body>
-                <blockquote className="blockquote mb-0">
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante. </p>
-                    <footer className="blockquote-footer">
-                        Someone famous in <cite title="Source Title">Source Title</cite>
-                    </footer>
-                </blockquote>
+            <Card.Body className="fondoLista">
+                <p></p>
             </Card.Body>
         </Card>
     );
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import './formulario.css'
 import Citas from './Citas'
 
@@ -12,18 +14,33 @@ const Formulario = () => {
                 <Card.Header><h5>Llenar el formulario para crear una cita</h5></Card.Header>
                 <Card.Body className="fondoFormulario">
                     <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+                        <Form.Group as={Row} className="mb-3" controlId="formNombreMascota">
+                            <Form.Label column sm="3" className="text-start">Nombre Mascota:</Form.Label>
+                            <Col sm="9">
+                                <Form.Control type="text" placeholder="nombre de mascota" />
+                            </Col>
                         </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                        <Form.Group as={Row} className="mb-3" controlId="formNombreDuenio">
+                            <Form.Label column sm="3" className="text-start">Nombre Dueño:</Form.Label>
+                            <Col sm="9">
+                                <Form.Control type="text" placeholder="Nombre de dueño" />
+                            </Col>
                         </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
+                        <Form.Group as={Row} className="mb-3" controlId="formFecha">
+                            <Form.Label column sm="3" className="text-start">Fecha:</Form.Label>
+                            <Col sm="3">
+                                <Form.Control type="date" placeholder="dd/mm/yyyy" />
+                            </Col>
+                            <Form.Label column sm="2" className="textJustify">Hora:</Form.Label>
+                            <Col sm="3">
+                                <Form.Control type="time" placeholder="hh:mm" />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3" controlId="formSintomas">
+                            <Form.Label column sm="3" className="text-start">Sintomas:</Form.Label>
+                            <Col sm="9">
+                                <Form.Control type="email" placeholder="describir los sintomas" />
+                            </Col>
                         </Form.Group>
                     </Form>
                 </Card.Body>
