e3386b11915a9f130e855f2a26aa9b297233df6e
carlosmsx <carlosmsx@gmail.com>
2022-09-06 22:53:35 -0300

    maquetado del titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Formulario from './components/Formulario';
 
 const App = () => {
     return (
-        <div>
-            
+        <div className='container-fluid'>
+            <h1 className='display-6 text-center'>Administrador pacientes de veterinaria</h1>
+            <Formulario/>
         </div>
     );
 };
